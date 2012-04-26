36bbed8400421cf4d2fedae4211a7b23b1f7e155
Robert Smith <r2theob23@gmail.com>
2012-04-26 11:46:23 -0400

    added first files to the repo..for the third time...

---
+++ Smith_Robert_assignment4.js
@@ -0,0 +1,24 @@
+var lib = function (){
+	var number = function ( otherNum ){
+		return libORob.number = otherNum.toFixed(2)
+};
+
+	var otherNum = function (argument) {
+		// body...
+	}
+
+return{
+	"number": number,
+	"otherNum"; otherNum,
+
+};
+
+var lib = libORob();
+
+lib.number(2.1);
+
+lib.otherNum();
+
+colsole.log(lib);
+
+
