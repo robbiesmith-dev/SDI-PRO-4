03ce7af4d29580e651fcb1b07b92af1e674851b7
Robert Smith <r2theob23@gmail.com>
2012-04-26 14:32:45 -0400

    added two functions to code

--- Smith_Robert_assignment4.js
+++ Smith_Robert_assignment4.js
@@ -1,24 +1,40 @@
 var lib = function (){
 	var number = function ( otherNum ){
 		return libORob.number = otherNum.toFixed(2)
-};
+	};
 
-	var otherNum = function (argument) {
-		// body...
-	}
+	var capitalizeString = function (str) {
+    var arr = str.split(/\s|_/);
+    for(var i=0,l=arr.length; i<l; i++) {
+        arr[i] = arr[i].substr(0,1).toUpperCase() + 
+                 (arr[i].length > 1 ? arr[i].substr(1).toLowerCase() : "");
+    	return lib.str("");
+    };
+    
+    var dateDiff = function(d1, d2) {
+        var t2 = d2.getTime();
+        var t1 = d1.getTime();
+		return parseInt((t2-t1)/(24*3600*1000));
+    };
+};
 
 return{
 	"number": number,
-	"otherNum"; otherNum,
-
+	"capitalizeString": capitalizeString,
+	"dateDiff": dateDiff,
 };
 
 var lib = libORob();
 
 lib.number(2.1);
 
-lib.otherNum();
+lib.capitalizeString();
 
-colsole.log(lib);
+lib.dateDiff();
+
+}
+
+
+console.log(lib);
 
 
