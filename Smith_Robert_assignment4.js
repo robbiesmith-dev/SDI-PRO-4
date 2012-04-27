7f13350e9d160bbdffe8dd0e3206f248f076299c
Robert Smith <r2theob23@gmail.com>
2012-04-26 20:46:04 -0400

    cleaned up code/added coments

--- Smith_Robert_assignment4.js
+++ Smith_Robert_assignment4.js
@@ -1,7 +1,9 @@
 var libORob = function (){
+	//Format a # to use a specific # of decimal places
 	var number = function ( otherNum ){
 		return libORob.number = otherNum.toFixed(2)
 	};
+	//Title-case a string 
 	var capitalizeString = function (str) {
     var arr = str.split(/\s|_/);
     for(var i=0,l=arr.length; i>l; i++) {
@@ -10,6 +12,7 @@ var libORob = function (){
     	return lib.capitalizeString("");
     	};
     };
+    //Find the total value of just the numbers in an array, even if some of the items are not numbers.
     var numSum = function (array){
 		 for(var i = 0; i < array.length; i++) {
           	q = (array[i]);
@@ -18,16 +21,19 @@ var libORob = function (){
     };
     		return lib.sum;
     };
+    //Find the number of hours or days difference between two dates.
 	var daysBetween = function(d1, d2) {
         var t2 = d2.getTime();
         var t1 = d1.getTime();
  
         return parseInt((t2-t1)/(24*3600*1000));
     };
+    //Does a string follow an aaa@bbb.ccc pattern like an email address?
     var validateEmail = function(elementValue){
     	var emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
     	return emailPattern.test(elementValue);  
     };
+    //Does a string follow a 123-456-7890 pattern like a phone number?
     var validatePhone = function(phoneDigits){
     	if (/^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/){
     		return lib.phoneDigits = true
